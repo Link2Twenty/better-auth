aa5144a3da4522d38f5f46b8fee02eabefbd87b0
Andrew Bone <Andrew.Bone@bloc.digital>
2026-02-09 10:35:16 +0000

    ✨ add config data

--- server/src/bootstrap.ts
+++ server/src/bootstrap.ts
@@ -1,7 +1,13 @@
 import type { Plugin } from '@strapi/types';
 
-const bootstrap: Plugin.LoadedPlugin['bootstrap'] = () => {
-  // bootstrap phase
+const bootstrap: Plugin.LoadedPlugin['bootstrap'] = async () => {
+  const config = strapi.documents('plugin::better-auth.better-auth-config');
+  const existingConfig = await config.count({});
+
+  // If no configuration exists, create a default one
+  if (!existingConfig) {
+    await config.create({ enabled: false, enforce: false, issuer: 'Strapi' } as any);
+  }
 };
 
 export default bootstrap;
--- server/src/content-types/index.ts
+++ server/src/content-types/index.ts
@@ -1,11 +1,14 @@
 import mfaToken from './mfa';
 import mfaTemp from './temp-mfa';
+import config from './config';
 
 import type { Plugin } from '@strapi/types';
 
 const contentTypes: Plugin.LoadedPlugin['contentTypes'] = {
   'mfa-token': mfaToken as unknown as Plugin.LoadedPlugin['contentTypes']['mfa-token'],
   'mfa-temp': mfaTemp as unknown as Plugin.LoadedPlugin['contentTypes']['mfa-temp'],
+  'better-auth-config':
+    config as unknown as Plugin.LoadedPlugin['contentTypes']['better-auth-config'],
 };
 
 export default contentTypes;
